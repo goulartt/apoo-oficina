0a9a1fb3bc408a44c167afd41951c5a0e925da3b
Joao <jv.goulart.almeida@hotmail.com>
2017-10-20 21:31:02 -0200

    colocando icone no projeto

--- oficina/src/main/java/application/Main.java
+++ oficina/src/main/java/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import tattool.rest.consume.UserRest;
@@ -19,6 +20,7 @@ public class Main extends Application {
 			new UserRest().verificaAdmin();
 			/*scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());*/
 			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/telas/icone.png")));
 			primaryStage.setResizable(false);
 			primaryStage.setTitle("Login");
 			primaryStage.show();
